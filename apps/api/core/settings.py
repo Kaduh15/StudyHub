ef9030c6cc086ccbc708c11fd1068e430ee8ac61
Kaduh15 <kadu.silva2014@gmail.com>
2025-10-31 09:38:40 -0300

    feat(api): adicionar STATIC_ROOT e ajustar caminho do banco de dados
    - Configura STATIC_ROOT para collectstatic funcionar corretamente
    - Padroniza formatação do caminho do banco de dados SQLite

--- apps/api/core/settings.py
+++ apps/api/core/settings.py
@@ -122,7 +122,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / 'db' / "db.sqlite3",
+        "NAME": BASE_DIR / "db" / "db.sqlite3",
     }
 }
 
@@ -162,6 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
